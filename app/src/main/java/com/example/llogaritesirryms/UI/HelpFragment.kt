e0d591ea148faf40d8e715c9be879d6cdbdbaefd
FatrionSadiku <fatrion.sa@gmail.com>
2022-12-15 14:59:09 +0100

    minor adjustments

--- app/src/main/java/com/example/llogaritesirryms/UI/HelpFragment.kt
+++ app/src/main/java/com/example/llogaritesirryms/UI/HelpFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.llogaritesirryms.R
 import com.example.llogaritesirryms.databinding.HelpFragmentBinding
 
 class HelpFragment : Fragment() {
@@ -20,6 +22,8 @@ class HelpFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_helpFragment_to_calcFragment)
+        }
     }
 }
